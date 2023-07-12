ebacf32fdc7af0aadcef8897fd6b63774e223681
Ibrahim Muneer <Ibrahim.muneer@d-p-s.com>
2023-07-12 15:30:14 +0500

    Add Assignments folder

--- Assignments/3/nameCases.js
+++ Assignments/3/nameCases.js
@@ -1,32 +1,26 @@
 "use strict";
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 var name = "ibrahim Muneer";
 function nameCases() {
     var lowerCase = name.toLowerCase();
     var upperCase = name.toUpperCase();
-    return "Name in lower case: ".concat(lowerCase, "\nName in upper case: ").concat(upperCase);
+    var titleCased = titleCase();
+    return "Name in lower case: ".concat(lowerCase, "\nName in upper case: ").concat(upperCase, "\nName in title case: ").concat(titleCased);
 }
 ;
 function titleCase() {
-    var myString = name.toLowerCase();
-    var myArr = myString.split(' ');
-    var nameInitials = myArr.map(function (name) {
-        return name[0].toUpperCase();
-    });
-    var titleCaseName = myArr.map(function (word) {
-        return word.split('');
-    });
-    var _a = __spreadArray([], titleCaseName, true), a = _a[0], b = _a[1];
-    console.log(a, b);
+    return name
+        .toLowerCase() //Converting to lower case
+        .split(' ') //Splitting name into parts: returns an array
+        .map(function (nme) {
+        return nme.charAt(0).toUpperCase() + nme.slice(1);
+    }) /*  1. Loops over the array returned by the split method.
+                2. gets first character of each element.
+                3. converts the character to upper case
+                4. slices each element of the array from 2nd character
+                5. concatenates uppercased letter with sliced string
+                6. returns new array*/
+        .join(' '); // joins the returned array by map method and then the function is returning complete title cased name.
 }
 ;
 titleCase();
--- Assignments/3/nameCases.ts
+++ Assignments/3/nameCases.ts
@@ -1,22 +1,25 @@
 const name:string = `ibrahim Muneer`;
 
 function nameCases() {
-    const lowerCase = name.toLowerCase();
-    const upperCase = name.toUpperCase();
-    return `Name in lower case: ${lowerCase}\nName in upper case: ${upperCase}`;
+    const lowerCase:string = name.toLowerCase();
+    const upperCase:string = name.toUpperCase();
+    const titleCased:string = titleCase();
+    return `Name in lower case: ${lowerCase}\nName in upper case: ${upperCase}\nName in title case: ${titleCased}`;
 };
 
 function titleCase() {
-    let myString:string = name.toLowerCase();
-    let myArr:string[] = myString.split(' ');
-    let nameInitials:string[] = myArr.map(name => {
-        return name[0].toUpperCase();
-    });
-    let titleCaseName = myArr.map(word=> {
-        return word.split('');
-    });
-    let [a, b] = [...titleCaseName];
-    console.log(a,b);
+    return name
+    .toLowerCase()  //Converting to lower case
+    .split(' ')     //Splitting name into parts: returns an array
+    .map(nme => {
+        return nme.charAt(0).toUpperCase() + nme.slice(1);
+    })      /*  1. Loops over the array returned by the split method.
+                2. gets first character of each element.
+                3. converts the character to upper case
+                4. slices each element of the array from 2nd character
+                5. concatenates uppercased letter with sliced string
+                6. returns new array*/
+    .join(' '); // joins the returned array by map method and then the function is returning complete title cased name.
 };
 
 titleCase();
