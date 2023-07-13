efc8ca8ca29558636e5523ac819e6aff136f7552
ibrahimMuneer <50015851+ibrahimMuneer@users.noreply.github.com>
2023-07-14 00:21:45 +0500

    Delete Assignments/Assignments directory

--- Assignments/Assignments/4/famousQuote.ts
+++
@@ -1,8 +0,0 @@
-const narratedBy = "Narrated Ibn 'Umar";
-const reference = "Reference	 : Sahih al-Bukhari 8\nIn-book reference	 : Book 2, Hadith 1";
-
-const quote = `\nNarrated Ibn 'Umar:\n\n"Allah's Messenger (ﷺ) said: Islam is based on (the following) five (principles):\n1. To testify that none has the right to be worshipped but Allah and Muhammad is Allah's Messenger (ﷺ).\n2. To offer the (compulsory congregational) prayers dutifully and perfectly.\n3. To pay Zakat (i.e. obligatory charity).\n4. To perform Hajj. (i.e. Pilgrimage to Mecca)\n5. To observe fast during the month of Ramadan."\n\nReference	 : Sahih al-Bukhari 8\nIn-book reference	 : Book 2, Hadith 1`;
-console.log(quote);
-
-
-export{};
--- Assignments/Assignments/6/strippingNames.ts
+++
@@ -1,6 +0,0 @@
-const name: string = "   \n Ibrahim Muneer   \t";
-
-console.log(name);
-console.log(name.trim());
-
-export{};
