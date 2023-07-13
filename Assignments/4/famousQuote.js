687caf3e88de19348674f6c1e269eb6f40c9549d
ibrahimMuneer <50015851+ibrahimMuneer@users.noreply.github.com>
2023-07-13 18:58:31 +0500

    Add files via upload

---
+++ Assignments/4/famousQuote.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var narratedBy = "Narrated Ibn 'Umar";
+var reference = "Reference	 : Sahih al-Bukhari 8\nIn-book reference	 : Book 2, Hadith 1";
+var quote = "\nNarrated Ibn 'Umar:\n\n\"Allah's Messenger (\uFDFA) said: Islam is based on (the following) five (principles):\n1. To testify that none has the right to be worshipped but Allah and Muhammad is Allah's Messenger (\uFDFA).\n2. To offer the (compulsory congregational) prayers dutifully and perfectly.\n3. To pay Zakat (i.e. obligatory charity).\n4. To perform Hajj. (i.e. Pilgrimage to Mecca)\n5. To observe fast during the month of Ramadan.\"\n\nReference\t : Sahih al-Bukhari 8\nIn-book reference\t : Book 2, Hadith 1";
+console.log(quote);
---
+++ Assignments/7/numberEight.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("".concat(16 / 2));
+console.log("".concat(2 * 4));
+console.log("".concat(4 + 4));
+console.log("".concat(16 - 8));
