a3541b14600eec17d09676f253d8cc55bde21ef2
ibrahimMuneer <50015851+ibrahimMuneer@users.noreply.github.com>
2023-07-14 11:31:03 +0500

    Delete Assignments/9 directory

--- Assignments/9/addingComments.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/*This is a multi line comment,
-I already have added some comments in my previous exercises.
-*/
-Object.defineProperty(exports, "__esModule", { value: true });
--- Assignments/9/addingComments.ts
+++
@@ -1,8 +0,0 @@
-/*This is a multi line comment,
-I already have added some comments in my previous exercises.
-*/
-
-//Single line comment: My name is Ibrahim Muneer, Date is 13-07-2023;
-
-
-export{};
