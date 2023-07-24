51f70d97d37286eac43585af5e456d1cd8773574
ibrahimMuneer <50015851+ibrahimMuneer@users.noreply.github.com>
2023-07-24 15:04:29 +0500

    Add files via upload

---
+++ MissingNumbersIndexes/missingNumbersIndexes.js
@@ -0,0 +1,22 @@
+"use strict";
+let myArr = [21, 25, 27, 29, 32, 32, 34, 40];
+// [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 32, 33, 34, 35, 36, 37, 38, 39, 40]
+let findIndex = (arr) => {
+    let ind = [];
+    let diff;
+    let count = 0;
+    myArr.map((num, i) => {
+        if (arr[i + 1]) {
+            diff = (arr[i + 1] - num) - 1;
+            if (diff > 0) {
+                for (let x = 1; x <= diff; x++) {
+                    ind.push(count + x);
+                }
+            }
+            count = ind[ind.length - 1] + 1;
+        }
+        ;
+    });
+    return `Missing numbers indexes are ${ind}`;
+};
+console.log(findIndex(myArr));
